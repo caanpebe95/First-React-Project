c2f84e7c55c0c6a5245d574dda791f3a64c9f2d7
caanpebe95 <capb9511@gmail.com>
2022-02-25 19:37:33 -0500

    Jason added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import quotes from './quotes.json'
 
 function App() {
+  const quote = quotes[0];
   return (
     <div className="App">
-      
+      <h1>{quote.quote}</h1>
+      <p>{quote.author}</p>
     </div>
   );
 }
