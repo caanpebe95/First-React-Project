060e056e475d9e78e0ba9bb2383a7660ae28b5c9
caanpebe95 <capb9511@gmail.com>
2022-03-04 00:13:23 -0500

    final update

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import './App.css';
 import QuoteBox from './Components/QuoteBox';
 
 
--- src/Components/QuoteBox.js
+++ src/Components/QuoteBox.js
@@ -1,11 +1,12 @@
-import React from 'react';
 import quotes from '../quotes.json'
 import '../App.css';
 import { useState } from 'react';
 
     const shuffle = () => Math.floor(Math.random() * quotes.length)
 
-    const colors = ["#845EC2", "#00C9A7", "#C34A36", "#B0A8B9", "#9B89B3", "#008BC6", "#926D00","#FF9671","#FFC75F"];
+    const colors = ["#845EC2" , "#00C9A7" , "#C34A36" , "#B0A8B9" , "#9B89B3" , "#008BC6" , "#926D00" , "#FF9671", "#FFC75F"];
+
+
 
 const QuoteBox = () => {
 
@@ -15,18 +16,20 @@ const QuoteBox = () => {
     //const quote = quotes[0];
 
     const changeQuote = () => {
-        setQuote(quotes[shuffle()])
-    }
 
-    document.body.style = `background: ${colors[Math.floor(Math.random() * 9)]}`
+        setQuote(quotes[shuffle()]);
+
+    };
+
+    const color = colors[Math.floor(Math.random() * colors.length)];
+
+    document.body.style.background = `${color}`;
 
     return (
-        <div class="QuoteBox">
+        <div class="QuoteBox" style={{color:color}}>
             <h1>{quote.quote}</h1>
             <p>{quote.author}</p> 
-            <button onClick={changeQuote}>
-            <i className="fa-solid fa-shuffle"></i>
-            </button>
+            <button onClick={changeQuote} style={{background:color}}></button>
         </div>
     );
 };
